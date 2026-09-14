9f251656b028b334cd1defe89457c5d8aabe9020
puskesmasgirimulyosatu <data.girisatu@gmail.com>
2026-09-14 14:24:57 +0700

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,25 @@
+{
+  "name": "e-Portal Digital Puskesmas Girimulyo I",
+  "short_name": "e-Portal G1",
+  "description": "Portal Terintegrasi Layanan Digital Puskesmas Girimulyo I",
+  "start_url": "./",
+  "scope": "./",
+  "display": "standalone",
+  "orientation": "portrait-primary",
+  "background_color": "#101913",
+  "theme_color": "#1F7A4D",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "any maskable"
+    }
+  ]
+}
